59211cae37a44995bfc4129747dd7c8fd1fa476b
Guoqi Sun <sun412926@gmail.com>
2024-10-05 22:22:24 +0800

    feat: custom my blog

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,31 @@
 export const site = {
-  name: "Astro Air",
+  name: "小孙同学",
   favicon: "/avatar.png",
-  title: "Astro Air",
-  url: "https://astro-air.netlify.app",
-  description: "minimal, personal blog theme for Astro",
+  title: "小孙同学",
+  url: "https://blog.sunguoqi.com",
+  description:
+    "🖥️ 前端小学生(React)｜📸 摄影爱好者(Nikon Zfc)｜🛸 旅行探索家（体验派）｜🚴 骑行蹭风选手（ Java 鱼雷 6-top ）｜🍎 科技产品发烧友（苹果&小米）<br/><br/> ✨ 路虽远行则将至，事虽难做则必成。热爱可抵岁月漫长。山高路远，独善其身，看世界，也找自己。希望能成为一个有趣的人～",
 }
 
 export const tabs = [
   {
-    label: "Archive",
+    label: "归档",
     link: "./archive",
   },
   {
-    label: "Tags",
+    label: "标签",
     link: "./tags",
   },
   {
-    label: "About",
+    label: "运动",
+    link: "https://cycling.sunguoqi.com",
+  },
+  {
+    label: "摄影",
+    link: "https://camlife.cn",
+  },
+  {
+    label: "关于",
     link: "./about",
   },
 ]
